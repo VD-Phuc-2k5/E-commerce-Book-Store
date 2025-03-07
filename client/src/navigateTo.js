a789e33b3b0a3ca4f3ca454455c5334900b1138e
duyvo2005 <duyvo1954@gmail.com>
2025-03-07 15:10:18 +0700

    navigateTo.js

---
+++ client/src/navigateTo.js
@@ -0,0 +1,12 @@
+// Page navigation
+export function navigateTo(page) {
+  document.getElementById("homePage").style.display = "none";
+  document.getElementById("catalogPage").style.display = "none";
+  document.getElementById("bookDetail").style.display = "none";
+  document.getElementById("checkoutPage").style.display = "none";
+  document.getElementById("orderSuccessPage").style.display = "none";
+
+  document.getElementById(page).style.display = "block";
+
+  window.scrollTo(0, 0);
+}
