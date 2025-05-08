59080e4a3b76557e80acf43baeb174e96527dda8
duyvo2005 <duyvo1954@gmail.com>
2025-05-08 17:54:06 +0700

    feat: add load carousel to navbar in app.js

--- design/src/app.js
+++ design/src/app.js
@@ -1,7 +1,11 @@
 import ComponentLoader from "./modules/loader-component.js";
+import carouselLoader from "./modules/loader-carousel.js";
 
 function App() {
   customElements.define("component-loader", ComponentLoader);
+  window.addEventListener("component-loaded", () => {
+    carouselLoader(20, 7000);
+  });
 }
 
 App();
