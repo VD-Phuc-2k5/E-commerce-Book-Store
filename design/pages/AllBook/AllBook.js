a3234b19bce990dcc93831a1a8580186bdebd31d
duyvo2005 <duyvo1954@gmail.com>
2025-06-07 16:16:50 +0700

    feat: insert all book page

--- design/pages/AllBook/AllBook.js
+++ design/pages/AllBook/AllBook.js
@@ -1,3 +1,28 @@
 import lazyLoadImg from "../../src/modules/lazyloadImg.js";
+import createProduct from "../../src/modules/createProductDom.js";
+import pagination from "../../src/modules/pagination.js";
 
 lazyLoadImg();
+
+const booksContainer = document.querySelector("#booksContainer");
+
+function renderBooks(books, selector) {
+  selector.innerHTML = "";
+
+  books.forEach((book) => {
+    const bookDOM = createProduct(
+      book.id,
+      book.imgUrl,
+      book.title,
+      book.author,
+      book.cost,
+      book.description,
+      ["col-lg-6", "col-12"]
+    );
+    selector.appendChild(bookDOM);
+  });
+}
+
+if (booksContainer) {
+  pagination(27, "/books", renderBooks);
+}
