9e1e9d464fe0d65a758f2476f07de7f459047b89
dont-wait <nguyentansang15042005@gmail.com>
2025-06-09 21:41:36 +0700

    style: text center for content title in mobile

--- design/pages/Blog/modules/draft.js
+++ design/pages/Blog/modules/draft.js
@@ -1,21 +1,34 @@
 /**
- * Initialize TinyMCE editor with all configurations
+ * Khởi tạo TinyMCE editor cho modal viết bài
+ * Đảm bảo gọi lại hàm này mỗi lần modal mở (sau khi textarea xuất hiện trong DOM)
+ * Đảm bảo selector trùng với id của textarea
  */
 function initializeTinyMCE() {
+    // Nếu đã có instance TinyMCE thì remove trước khi init lại
+    if (window.tinymce && tinymce.get('blog-content')) {
+        tinymce.get('blog-content').remove();
+    }
     tinymce.init({
-        selector: '#blog-editor',
+        selector: '#blog-content',
+        height: 500,
         plugins: [
-            'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
+            'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link',
+            'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount'
         ],
-        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | ' +
+            'link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | ' +
+            'align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
         tinycomments_mode: 'embedded',
         tinycomments_author: 'Author name',
         mergetags_list: [
             { value: 'First.Name', title: 'First Name' },
-            { value: 'Email', title: 'Email' },
+            { value: 'Email', title: 'Email' }
         ],
-        ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
-        height: 500
+        skin: 'oxide',
+        content_css: 'default',
+        branding: false,
+        menubar: false,
     });
 }
-export default initializeTinyMCE
+
+export default initializeTinyMCE;
